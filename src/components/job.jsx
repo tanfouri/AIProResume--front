090cbcc707478113a6bd77be90866ef4b345b0e3
tanfouri <tanfouri.mohamed.marwan@gmail.com>
2024-01-16 14:35:29 +0100

    AIProResume

---
+++ src/components/job.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+//import "./public.css";
+
+const Job = () => {
+  const sites = [
+    {
+      title: "Tanit jobs",
+      iframeUrl: "https://www.tanitjobs.com/",
+    },
+    {
+      title: "Linkedin",
+      iframeUrl: "https://www.linkedin.com/",
+    },
+   
+ 
+  ];
+
+  const [expandedCards, setExpandedCards] = useState([]);
+
+  return (
+    <div className="form-style-public">
+      <div className="row justify-content-center">
+        {sites.map((site, index) => (
+          <div
+            className={`col-lg-6 col-md-6 ${
+              expandedCards.includes(index) ? "col-sm-12 expanded" : ""
+            }`}
+            key={index}
+            style={{ margin: "10px" }} // Add margin to create space between cards
+          >
+            <div className="card">
+              <div className="card-body">
+             
+                <div className="embed-responsive embed-responsive-16by9">
+                  <iframe
+                    title={`iframe-${index}`}
+                    className="embed-responsive-item"
+                    src={site.iframeUrl}
+                    allowFullScreen
+                  ></iframe>
+                </div>
+                <div className="navigation-buttons">
+                  <a
+                    href={site.iframeUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Aller sur le site
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Job;
